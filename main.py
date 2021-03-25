092b48fd50f53584be942aa5053f75adddb98063
Vishal Kumar <vishalekumar2006@gmail.com>
2021-03-25 16:15:31 +0530

    Added basic GUI
    @AnkithAbhayan , i added the basic GUI 
    edit it if you feel the colors are not good
    also the colors are not final, i  was just messing around

--- main.py
+++ main.py
@@ -1,2 +1,47 @@
 import keyboard
-print("Typing Speed test V0.1")
+print('Hi ankith i\'m deleting this line of code')
+import tkinter
+# from tkinter import Label
+# from tkinter import Entry
+# from tkinter import Tk
+from tkinter import*
+
+# The main window
+win = Tk()
+win.geometry("500x500+200+200")
+win.title("Typing test")
+
+# Then entry box
+
+data_of_theEntry = StringVar()
+Entry_of_text = Entry(win, bg ="#808080",fg = "#7DF9FF" ,font = ("Verdana","30","bold","italic") , relief = GROOVE,textvariable = data_of_theEntry,  )
+Entry_of_text.pack(fill = X)
+Entry_of_text.place(x = 100, y = 450, height = 200,width = 1200)
+
+# Some display Labels
+
+a_label_1 = Label(win, fg = "black", font = ("Verdana",30,"bold","italic"), text = "Test your Typing speed here!")
+a_label_1.place(x = 400, y = 50)
+
+
+#Commands
+
+def instructions_command():
+    win2 = Tk()
+    win2.title("Instructions")
+    win2.geometry("400x200")
+    win2.resizable(width=0,height=0)
+
+    label_of_small_window = Label(win2,text = "Instructions:\n A Random word will be displayed on your screen. \n What you're going to do is, type that word in the textbox and hit \"enter\" \n There will be 30 words. \n You can increase the amount by your choice. \n Your result will be displayed after the timer ends!")
+    label_of_small_window.place(x = 0,y = 0)
+
+
+    win2.mainloop()
+
+
+# Buttons --------------------------------------------------------------------
+
+instructions_btn = Button(win, text="Instructions(click)",command = instructions_command , font = ("Verdana",20,'bold','italic'), relief = RIDGE, border = 0,activebackground = "#FFA500")
+instructions_btn.place(x = 0, y = 100)
+
+win.mainloop()
